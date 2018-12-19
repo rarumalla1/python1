b481aafa40702006e8af74ef212c75448fff3d29
Raghu Ram Arumalla <30503562+rarumalla1@users.noreply.github.com>
2018-12-19 08:21:57 -0500

    initial commit

---
+++ py1.py
@@ -0,0 +1,25 @@
+import math
+import calendar
+from builtins import print
+
+a = math.sqrt(81)
+print(a)
+#help(math.factorial)
+print(math.factorial(3))
+
+from math import factorial as fac
+print(fac(4))
+
+
+c = 'pooji'
+c.capitalize()
+print(c.capitalize())
+
+
+aa = "aa bb cc dd ee ff"
+data = aa.encode('encodings.utf_8')
+data
+
+
+
+
